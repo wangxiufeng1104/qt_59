4803bc64ace79cd194c0408874f414414c23d109
王秀峰 <33338160+wangxiufeng1104@users.noreply.github.com>
2023-04-09 16:31:22 +0800

    Update qperson.h

--- QPerson/qperson.h
+++ QPerson/qperson.h
@@ -6,9 +6,11 @@
 class QPerson : public QObject
 {
     Q_OBJECT
+    // 这个宏为类提供额外的信息，要想获取这个Q_CLASSINFO这个信息要使用QObject::metaObject()
     Q_CLASSINFO("author", "xiufeng")
     Q_CLASSINFO("company", "UPC")
     Q_CLASSINFO("version", "1.0.0")
+    // Q_PROPERTY()是一个宏，用来在一个类中声明一个属性property，由于该宏是qt特有的，需要用moc进行编译，故必须继承于QObject类
     Q_PROPERTY(int age READ getAge WRITE setAge NOTIFY ageChanged)
     Q_PROPERTY(QString name MEMBER m_name)
     Q_PROPERTY(int score MEMBER m_score)
